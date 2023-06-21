f7427bead6393f77616b3ec812772ee73e2158de
Salman Tubagus Alfarizi <110708002+Salman699@users.noreply.github.com>
2023-06-21 14:15:32 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -79,7 +79,7 @@ $role = Auth::user()->role ?? null
         </div>
     </div>
 
-    <div class="col-md-4">
+<!--     <div class="col-md-4">
         <div class="card" style="width: 18rem;">
             <div class="card-body">
                 <h1 class="card-title text-primary">Video</h1>
@@ -90,7 +90,7 @@ $role = Auth::user()->role ?? null
                 <a href="/video" class="card-link"> Video </a>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 @endif
 @endsection
